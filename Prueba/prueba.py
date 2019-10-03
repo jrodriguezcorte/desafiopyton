4ecc464eaa8247e84f2fb07f55e6741d28d6913f
Jonathan Rodriguez <jrodriguezcorte@gmail.com>
2019-10-02 21:22:35 -0300

    python

--- Prueba/prueba.py
+++ Prueba/prueba.py
@@ -1,5 +1,6 @@
 import json
 import requests
+from itertools import groupby
 
 def request(url,api_key):
     headers = headers = {
@@ -24,7 +25,7 @@ def build_web_page(diccionario):
     longitud = len(diccionario_fotos)
     for i in range(longitud):
         img = diccionario_fotos[i]['img_src']
-        html += "\t<li>><img src=\"{}\"></li>\n".format(img)
+        html += "\t<li><img src=\"{}\"></li>\n".format(img)
 
     html += "</ul>\n"
     html += "</body>\n"
@@ -33,8 +34,21 @@ def build_web_page(diccionario):
     with open("prueba.html", "w") as f:
         f.write(html)
 
+def photos_count(diccionario):
+    diccionario_fotos = diccionario['photos']
+    longitud = len(diccionario_fotos)
+    lista_camara = []
+    for i in range(longitud):
+        nombre_camara = diccionario_fotos[i]['camera']['name']
+        lista_camara.append(nombre_camara)
+
+    lista_camara.sort()
+    diccionario_final = {k: len(list(v)) for k, v in groupby(lista_camara)}
+    return diccionario_final
+
 api_key = 'rAQopkrvRyhC5hWChcJMlhkS1geKW2HeaSf7qzG4'
 url = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=100'
 
 diccionario = request(url,api_key)
 build_web_page(diccionario)
+photos_count(diccionario)
